e4f64241de762208c9ebd0f39d413eec3ded3be1
Daniel <danielgomesveiga@hotmail.com>
2022-11-28 11:09:40 -0300

    Corrigindo Requisitos

--- app/backend/src/services/UserService.ts
+++ app/backend/src/services/UserService.ts
@@ -6,7 +6,7 @@ import Users from '../database/models/User';
 export default class UserService {
   private _badRequest = 'All fields must be filled';
   private _regex = /\S+@\S+\.\S+/;
-  private _unauthorized = 'Unauthorized';
+  private _unauthorized = 'Incorrect email or password';
 
   constructor(
     private _JWT = new TokenJWT(),
@@ -18,21 +18,21 @@ export default class UserService {
 
     console.log(password.length);
     if (!email || !password) {
-      return { status: 400, message: this._badRequest };
+      return { status: 400, message: { message: this._badRequest } };
     }
 
     if (!this._regex.test(email)) {
-      return { status: 401, message: this._unauthorized };
+      return { status: 401, message: { message: this._unauthorized } };
     }
 
     const data = await this._userModel.findOne({ where: { email } });
-    if (data) {
-      const compare = compareSync(password, data.password);
-      if (!compare) {
-        return { status: 401, message: this._unauthorized };
-      }
+    if (!data) {
+      return { status: 401, message: { message: this._unauthorized } };
     }
-
+    const compare = compareSync(password, data!.password);
+    if (!compare) {
+      return { status: 401, message: { message: this._unauthorized } };
+    }
     const token = this._JWT.createToken(login);
     return { status: 200, message: { token } };
   };
